69a81057b64580db430885bad7b2f6c96a6f49fc
Adão Ribeiro <adao-eduardo@hotmail.com>
2023-02-03 17:28:40 -0300

    att

--- src/pages/components/selectPlan/index.tsx
+++ src/pages/components/selectPlan/index.tsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { MutableRefObject, useContext, useRef } from "react";
 import { IMyContextProps, MyContext } from "../../../context/mycontext";
 import { TypePlan } from "../../../interfaces/IPlan";
 import styles from "./styles.module.scss"
 
 export function SelectPlan() {
     const { step, setStep, person, setPerson }: IMyContextProps = useContext(MyContext);
-
+    const plan = person.plan.typePlan === TypePlan.MONTHLY ? "mo" : "yr";
     const flexSEnd = {
         "justifyContent": "flex-end"
+    }
+    const backgroundColorBlue = {
+        "backgroundColor": "var(--Magnolia)"
     }
     const handleBack = () => {
         setStep(1);
@@ -29,30 +32,48 @@ export function SelectPlan() {
             return { ...prev, plan: newPLan }
         })
     }
+    const handleSelectPlan = (name: string, price: string) => {
+        var newPlan = {
+            ...person.plan, name: name,
+            price: price
+        }
+
+        setPerson((prev) => {
+            return { ...prev, plan: newPlan }
+        })
+
+
+        console.log(person)
+    }
+
     return (
         <div className={styles.container}>
             <h1>Select your plan</h1>
             <span>You have the option of monthly or yearly billing.</span>
             <div className={styles.groupPlan}>
-                <div className={styles.groupBox} onMouseDown={() => console.log("click")}>
+                <div className={styles.groupBox} style={person.plan.name === "Arcade" ? backgroundColorBlue : null} onMouseDown={() => { handleSelectPlan("Arcade", "9") }} >
                     <img src="/assets/images/icon-arcade.svg"></img>
-                    <div className={styles.groupText}>
-                        <span>Arcade</span>
-                        <p>$9/mo</p>
+                    <div className={styles.groupText} >
+                        <h3>Arcade</h3>
+                        <p>$9/{plan}</p>
+                        {person.plan.typePlan === TypePlan.YEARLY && <span>2 months free</span>}
+
                     </div>
                 </div>
-                <div className={styles.groupBox} onMouseDown={() => console.log("click")}>
+                <div className={styles.groupBox} style={person.plan.name === "Advanced" ? backgroundColorBlue : null} onMouseDown={() => { handleSelectPlan("Advanced", "12") }}>
                     <img src="/assets/images/icon-advanced.svg"></img>
                     <div className={styles.groupText}>
-                        <span>Advanced</span>
-                        <p>$12/mo</p>
+                        <h3>Advanced</h3>
+                        <p>$12/{plan}</p>
+                        {person.plan.typePlan === TypePlan.YEARLY && <span>2 months free</span>}
                     </div>
                 </div>
-                <div className={styles.groupBox} onMouseDown={() => console.log("click")}>
+                <div className={styles.groupBox} style={person.plan.name === "Pro" ? backgroundColorBlue : null} onMouseDown={() => { handleSelectPlan("Pro", "15") }}>
                     <img src="/assets/images/icon-pro.svg"></img>
                     <div className={styles.groupText}>
-                        <span>Pro</span>
-                        <p>$15/mo</p>
+                        <h3>Pro</h3>
+                        <p>$15/{plan}</p>
+                        {person.plan.typePlan === TypePlan.YEARLY && <span>2 months free</span>}
                     </div>
                 </div>
             </div>
@@ -60,8 +81,8 @@ export function SelectPlan() {
                 <span style={person.plan.typePlan == TypePlan.MONTHLY ? { color: "var(--Marine-blue)" } : null}>
                     Monthly
                 </span>
-                <button className={styles.btn} onClick={handleSelectTypePlan}>
-                    <div className={person.plan.typePlan == TypePlan.MONTHLY ? styles.divPendulumLeft : styles.divPendulumRight}></div>
+                <button className={styles.btn} style={person.plan.typePlan === TypePlan.MONTHLY ? null : flexSEnd} onClick={handleSelectTypePlan}>
+                    <div className={styles.divPendulumRight} ></div>
                 </button>
                 <span style={person.plan.typePlan == TypePlan.YEARLY ? { color: "var(--Marine-blue)" } : null}>
                     Yearly
